71a8517c3925faf0204d928b85f1f6fc0d91c42e
Charlie Hills <github.cb25@tmx.email>
2022-03-27 08:50:10 -0500

    Add book-building functionality to Book
    Before, the Book class was fairly simple: just attributes that make up a book with simple setters and getters.
    
    Now, the Book class has functionality. A new build() method was added to convert the basic attributes into a book. This is done with two key private methods, parse() and paginate().
    
    * parse() is responsible for taking the initial, raw, supplied text and breaking it into a clean structure: an array of lines (or paragraphs) and each line/paragraph is an array of words
    * paginate() loops through the parsed structure and mimic's the Minecraft in-game line and page breaks

--- src/Book.php
+++ src/Book.php
@@ -7,14 +7,27 @@ class Book
     private string $author;
     private int $generation;
     private string $display;
+
+    private array $parsedText;
     private array $page;
+
+    private array $strings;
 
     public function __construct(string $text = null)
     {
         if ($text !== null) {
             $this->setText($text);
         }
+
         $this->page = [];
+
+        $this->strings = [
+            40 => "!|;:','.i",
+            60 => "l",
+            80 => "[]tI\" ",
+            100 => "fk*(){}<>",
+            140 => "@",
+        ];
     }
 
     public function setText(string $text): Book
@@ -87,6 +100,132 @@ class Book
     public function getPages(): array
     {
         return $this->page;
+    }
+
+    public function build(): Book
+    {
+        $this->parse();
+        $this->paginate();
+        return $this;
+    }
+
+    private function parse(): Book
+    {
+        // Start with raw text
+        $text = $this->getText();
+
+        // Replace any CR+LF combinations with a newline
+        $text = str_replace("\r\n", "\n", $text);
+
+        // Replace any dangling CRs with a newline
+        $text = str_replace("\r", "\n", $text);
+
+        // Split file into lines
+        $lines = explode("\n", $text);
+
+        // Split lines into words
+        foreach ($lines as $line) {
+            $this->parsedText[] = explode(' ', $line);
+        }
+
+        return $this;
+    }
+
+    private function paginate(): Book
+    {
+        // Initialize tracking variables
+        $lines = 0;
+        $currentLine = '';
+        $currentLineLen = 0.0;
+        $currentPageNo = 0;
+        $currentPageText = '';
+
+        // Begin with a blank page (a bare minimum book has one page and no text)
+        $this->addPage();
+
+        // Loop through book text one line at a time
+        // These are hard returns (or paragraphs) part of the source text
+        foreach ($this->parsedText as $line) {
+
+            // Loop through lines one word at a time
+            foreach ($line as $word) {
+
+                // Running count of line length
+                $wordLength = $this->calcWordLength($word);
+                $currentLineLen += $wordLength;
+
+                // If the line length is too long, add to page and start counting a new line
+                if ($currentLineLen > 19) {
+
+                    $currentPageText .= $currentLine;
+
+                    $lines++;
+                    $currentLine = '';
+                    $currentLineLen = $wordLength;
+
+                    // If the number of lines is too long, start a new page
+                    if ($lines > 13) {
+                        $this->updatePage($currentPageNo, $currentPageText);
+                        $this->addPage();
+                        $lines = 0;
+                        $currentLine = '';
+                        $currentLineLen = 0;
+                        $currentPageNo++;
+                        $currentPageText = '';
+                    }
+                }
+
+                // Add word to current line
+                $currentLine .= $word . ' ';
+                $currentLineLen += $this->calcCharLen(' ');
+            }
+
+            // Add line to current page
+            $currentPageText .= $currentLine . '\\\n';
+            $this->updatePage($currentPageNo, $currentPageText);
+
+            $lines++;
+            $currentLine = '';
+            $currentLineLen = 0;
+
+            // If the number of lines is too long, start a new page
+            if ($lines > 13) {
+                $this->addPage();
+                $lines = 0;
+                $currentPageNo++;
+                $currentPageText = '';
+            }
+        }
+
+        return $this;
+    }
+
+    private function calcWordLength(string $word): float
+    {
+        $len = 0.0;
+        $chars = str_split($word);
+        foreach ($chars as $char) {
+            $len += $this->calcCharLen($char);
+        }
+        return $len;
+    }
+
+    private function calcCharLen($char): float
+    {
+        $len = 120.0;
+
+        if ($char === '') {
+            return 0.0;
+        } else {
+            foreach ($this->strings as $size => $collection) {
+                if (strpos($collection, $char) !== false) {
+                    $len = $size;
+                    break;
+                }
+            }
+        }
+
+        return $len / 120;
     }
 
 }
